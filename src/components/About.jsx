3dfecf488e02d6b0e884b78091d632b8ba63b898
c221105Nehad <nehadinkiadhossain@gmail.com>
2024-07-04 18:51:43 +0600

    portfolio setup done

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+
+const About = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default About
