b434675ac68bc59bc184920ad305c85537f8039c
c221105Nehad <nehadinkiadhossain@gmail.com>
2024-07-07 09:22:54 +0600

    About component done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,36 +1,42 @@
-// import React from 'react';
-// 
-// 
 
-// 
-// 
-// 
-
-// const About = () => {
-//   return (
-//     <>
-//       <motion.div variants={textVariant()}>
-//         <p className={styles.sectionSubText}>Introduction</p>
-//         <h2 className={styles.sectionHeadText}>Overview</h2>
-//       </motion.div>
-//     </>
-//   )
-// }
-
-// export default About;
 import React from "react";
 import { styles } from '../styles';
 import { motion } from 'framer-motion';
 import { fadeIn, textVariant } from '../utils/motion';
 import { Tilt } from 'react-tilt';
 import { services } from '../constants';
+import { SectionWrapper } from "../hoc";
 
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className='xs:w-[250px] w-full'>
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450
+          }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+        >
+          <img src={icon} alt={title} 
+               className="w-16 h-16 object-contain"
+          />
+          <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  )
+}
 const About = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
         <p className={styles.heroSubText}>Introduction</p>
-        <h3 className={styles.heroHeadText}>Overview</h3>
+        <h2 className={styles.heroHeadText}>Overview</h2>
       </motion.div>
 
       <motion.p
@@ -40,11 +46,13 @@ const About = () => {
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque molestiae recusandae est illum laboriosam quae fuga provident dolor odit iste! Pariatur quisquam dolor dolorum accusantium in ipsum. Labore minima accusantium impedit quos consequuntur accusamus sequi ipsam voluptates corporis doloribus amet at incidunt quo, expedita est quis provident deleniti cum cumque beatae repudiandae? Id laudantium distinctio cum illo amet laborum veritatis officia unde consequuntur mollitia, deserunt magnam temporibus debitis nostrum sunt.
       </motion.p>
 
-      <div>
-        
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
       </div>
     </>
   )
 }
 
-export default About
+export default SectionWrapper(About, "about");
